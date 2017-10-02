6050227477fbb5e3dff840fc8593a7a242fdd0a7
Rafael Gaia <rgaia@e1z2r3p22.42.us.org>
2017-10-01 18:56:39 -0700

    lstmap update

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: rgaia <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/29 13:37:06 by rgaia             #+#    #+#             */
-/*   Updated: 2017/09/29 23:54:34 by rgaia            ###   ########.fr       */
+/*   Updated: 2017/09/29 13:55:16 by rgaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,21 @@
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
 	t_list *new_lst;
-	
-	if (lst == NULL)
+	t_list *new;
+
+	if (!lst)
 		return (NULL);
-	new_lst = ft_lstnew(NULL, 0);
-	while (lst)
+	new_lst = f(lst);
+	new = new_lst;
+	while (lst->next)
 	{
-		ft_lstadd(&new_lst, f(lst));
 		lst = lst->next;
+		if (!(new_lst->next = f(lst)))
+		{
+			free(new_lst->next);
+			return (NULL);
+		}
+		new_lst = new_lst->next;
 	}
-	return (new_lst);
+	return (new);
 }
